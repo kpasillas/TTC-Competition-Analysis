f1f03ae51231280f560ebd4e5b79687d27667a75
Kristopher Pasillas <kris@kris-p.net>
2019-12-11 05:44:10 -0800

    Added blank update_gate1_data file

---
+++ update_gate1_data.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+import requests
+import bs4
+import re
+import csv
+from datetime import date
+from time import sleep
+from tqdm import tqdm
+
+def main():
+
+
+
+
+
+
+
+if __name__ == '__main__': main()
