a1c42a7e2409eeca1d7c1cb04b4c374e24046a6b
Kristopher Pasillas <kris@kris-p.net>
2020-01-16 13:21:12 -0800

    Added update_collette_data file

---
+++ update_collette_data.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+# from selenium.webdriver.common.keys import Keys
+# import requests
+import bs4
+# import re
+import csv
+from datetime import date
+# from datetime import datetime
+# from time import sleep
+from tqdm import tqdm
+
+def main():
+
+    today = date.today()
+    file_name = 'collette_data_{}.csv'.format(today.strftime("%m-%d-%y"))
+    
+    with open(file_name, 'a') as new_file:
+        csv_writer = csv.writer(new_file, lineterminator='\n')
+
+        field_names = ['Trip Name','Departure Date','field','value']
+        csv_writer.writerow(field_names)
+        # print(field_names)
+    
+        linkUS = (
+            'https://www.gocollette.com/en/tours/north-america/usa/national-parks/booking?b=1#step/1',
+            'https://www.gocollette.com/en/tours/north-america/usa/alaska-and-the-yukon/booking?b=1#step/1',
+        )
+
+        print("\n")
+
+        for link in tqdm(linkUS):
+        
+            driver = webdriver.Chrome()
+            driver.get(link)
+
+            nameElement = driver.find_element_by_tag_name('h3')
+            soup = bs4.BeautifulSoup(nameElement.get_attribute('innerHTML'), 'lxml')
+            trip_name = soup.contents[0].text.strip()
+            # print(trip_name)
+
+            try:
+                WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'date-group-dates')))
+
+                monthElements = driver.find_elements_by_class_name('date-group-dates')
+                for month in monthElements:
+
+                    departureElements = month.find_elements_by_class_name('date-group')
+                    for departure in departureElements:
+
+                        soup = bs4.BeautifulSoup(departure.get_attribute('innerHTML'), 'lxml')
+                        # file.write(soup.prettify())
+
+                        date_numbers = soup.find('div', class_='date').text.split()
+                        departure_date = '{:02}-{}-{}'.format(int(date_numbers[1].strip(',')), date_numbers[0], date_numbers[2])
+                        # print(departure_date)
+
+                        if soup.find('div', class_='date-alert'):
+                            notes = soup.find('div', class_='date-alert').text.strip()
+                            string_to_write = [trip_name,departure_date,'Notes',notes]
+                            csv_writer.writerow(string_to_write)
+                            # print(string_to_write)
+
+                        actual_price = soup.find('span', class_='discountedPrice').text.strip()
+                        string_to_write = [trip_name,departure_date,'Actual Price USD',actual_price]
+                        csv_writer.writerow(string_to_write)
+                        # print(string_to_write)
+
+                        if soup.find('span', class_='crossout'):
+                            original_price = soup.find('span', class_='crossout').text.strip()
+                        else:
+                            original_price = actual_price
+                        string_to_write = [trip_name,departure_date,'Original Price USD',original_price]
+                        csv_writer.writerow(string_to_write)
+                        # print(string_to_write)
+
+            finally:
+                driver.quit()
+
+    new_file.close()
+
+    print("\nDone!\n")
+
+if __name__ == '__main__': main()
