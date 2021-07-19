7d33bf813a4eac9086eb94067c89176fad514497
Kristopher Pasillas <kris@kris-p.net>
2021-07-19 14:07:21 -0700

    Revised update_globus_data file to work with Trip and Departure classes

--- update_globus_data.py
+++ update_globus_data.py
@@ -9,8 +9,11 @@ from bs4 import BeautifulSoup
 from tqdm import tqdm
 import requests
 import re
-import csv
 from datetime import datetime
+
+from trip import Trip
+from departure import Departure
+
 
 def main():
 
@@ -20,170 +23,130 @@ def main():
     regions_US = []
     trips_US = []
     regions_AU = []
+    trips = []
     error_log = dict()
 
-    trip_continents = [
+    trip_countries = [
         {'continent_name':'United States', 'US_link':'https://www.globusjourneys.com/Vacation-Packages/Tour-United-States/', 'AU_link':''},
         {'continent_name':'Canada', 'US_link':'https://www.globusjourneys.com/Vacation-Packages/Tour-Canada/', 'AU_link':''}
     ]
 
-    for continent in tqdm(trip_continents):
 
-        if continent['US_link']:
+    for country in tqdm(trip_countries):
+        
+        if country['US_link']:
             driver = webdriver.Chrome()
-            driver.get(continent['US_link'])
-
+            driver.get(country['US_link'])
+            
             try:
                 WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'explore-dest')))
-                trips = driver.find_elements_by_class_name('explore-dest')
-
-                for trip in trips:
-                    soup = BeautifulSoup(trip.get_attribute('innerHTML'), 'lxml')
+                regions = driver.find_elements_by_class_name('explore-dest')
+                
+                for region in regions:
+                    soup = BeautifulSoup(region.get_attribute('innerHTML'), 'lxml')
                     link = '{}{}'.format(link_prefix, soup.find('a').get('href'))
                     regions_US.append(link)
-
+                    
             finally:
                 driver.quit()
 
     regions_US.append('https://www.globusjourneys.com/Vacation-Packages/Tour-South-America/Central-America/')
     regions_US.append('https://www.globusjourneys.com/Vacation-Packages/Tour-Cuba/Vacations/')
 
-    # print('\n{}\n'.format(regions_US))
 
     for region in tqdm(regions_US):
+        
         driver = webdriver.Chrome()
         driver.get(region)
-
+        
         try:
             WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'product-list-contain')))
             tours = driver.find_elements_by_class_name('product-list-contain')
-
+            
             for tour in tours:
                 soup = BeautifulSoup(tour.get_attribute('innerHTML'), 'lxml')
                 title = soup.find('h3').text.strip()
                 link = '{}{}'.format(link_prefix, soup.find('div', class_='btn-red').find_parent('a').get('href'))
                 trips_US.append({'trip_name':title, 'link':link})
-
+                
         except TimeoutException:
             error_log['{} - US'.format(region)] = 'Non-List of Trips'
-
+            
         finally:
             driver.quit()
 
-    # print('\n{}\n'.format(trips_US))
 
-    for continent in tqdm(trip_continents):
-
-        if continent['AU_link']:
+    for country in tqdm(trip_countries):
+        
+        if country['AU_link']:
             driver = webdriver.Chrome()
-            driver.get(continent['AU_link'])
-
+            driver.get(country['AU_link'])
+            
             try:
                 WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'destinations__item')))
                 regions = driver.find_elements_by_class_name('destinations__item')
-
+                
                 for region in regions:    
                     soup = BeautifulSoup(region.get_attribute('innerHTML'), 'lxml')
                     link = soup.find('a').get('href')
                     regions_AU.append('{}.au{}'.format(link_prefix, link))
-
+                    
             finally:
                 driver.quit()
 
-    # print(regions_AU)
 
-    with open(file_name, 'a') as new_file:
-        csv_writer = csv.writer(new_file, lineterminator='\n')
+    for trip in tqdm(trips_US):
+        
+        departures = []
+        
+        res = requests.get(trip['link'])
+        soup = BeautifulSoup(res.text, 'lxml')
 
-        field_names = ['Trip Name','DepartureID','field','value']
-        csv_writer.writerow(field_names)
+        try:
+            trip_name = soup.find("h1").contents[0].text
+            trip_code = 'Globus{}'.format(soup.find("h1").contents[2].text.strip("()"))
+            
+            for departure in soup.find_all('div', class_='listing'):
+                
+                date_numbers = departure.find('p', class_='date-numbers').text.split()
+                departure_date = '{:02}-{}-20{}'.format(int(date_numbers[0]), date_numbers[1], date_numbers[2])
+                
+                actual_price_usd = departure.find('p', class_='price-actual').text.strip().replace(',', '')
+                if departure.find('p', class_='price-strike'):
+                    original_price_usd = departure.find('p', class_='price-strike').text.strip().replace(',', '')
+                else:
+                    original_price_usd = actual_price_usd
+                
+                if departure.find('div', class_='small-message'):
+                    type = 'Small-Group Discovery'
+                elif departure.find('div', class_='popular-message'):
+                    type = 'Popular'
+                else:
+                    type = ''
+                
+                if departure.find('div', class_='listing-status'):
+                    notes = departure.find('div', class_='listing-status').text.strip()
+                else:
+                    notes = ''
+                
+                if re.search( "Not Available", departure.find('div', class_='listing-buttons-contain').text) or notes == '0 Seats Remaining':
+                    status = 'Not Available'
+                    available = False
+                else:
+                    status = 'Available'
+                    available = True
+                    
+                new_dep = Departure(date = departure_date, actual_price_usd = actual_price_usd, original_price_usd = original_price_usd, type = type, notes = notes, status = status, available = available)
+                departures.append(new_dep)
+            
+            new_trip = Trip(trip_name, trip_code, departures)
+            trips.append(new_trip)
+        
+        except:
+            error_log['{} - US'.format(link)] = 'Missing from Website'
 
-        for trip in tqdm(trips_US):
-
-            res = requests.get(trip['link'])
-            soup = BeautifulSoup(res.text, 'lxml')
-
-            try:
-                trip_name = soup.find("h1").contents[0].text
-                code = soup.find("h1").contents[2].text
-                code = code.strip("()")
-                # print('{} - {}'.format(trip_name, code))
-                previous_departure_date = ''
-                duplicate_departure_count = 0
-
-                for departure in soup.find_all('div', class_='listing'):
-
-                    date_numbers = departure.find('p', class_='date-numbers').text.split()
-                    departure_date = '{:02}-{}-20{}'.format(int(date_numbers[0]), date_numbers[1], date_numbers[2])
-                    # print(departure_date)
-
-                    if departure_date == previous_departure_date:                   # check if duplicate departure
-                        duplicate_departure_count += 1
-                    else:
-                        duplicate_departure_count = 0
-
-                    departure_letter = str(chr(duplicate_departure_count + 97))
-                    day = '{:02}'.format(int(date_numbers[0]))
-                    month = str(chr((datetime.strptime(date_numbers[1], '%b')).month + 64))
-                    year = date_numbers[2][-2:]
-                    op_code = 'Globus{}{}'.format(code, year)
-                    departure_code = '{}{}{}{}'.format(day, month, year, departure_letter)
-                    departure_id = '{}-{}'.format(op_code, departure_code)
-                    # print(departure_id)
-
-                    string_to_write = [trip_name,departure_id,'DepartureDate',departure_date]
-                    csv_writer.writerow(string_to_write)
-                    # print(string_to_write)
-
-                    actual_price = departure.find('p', class_='price-actual').text.strip().replace(',', '')
-                    string_to_write = [trip_name, departure_id,'ActualPriceUSD',actual_price]
-                    csv_writer.writerow(string_to_write)
-                    # print(string_to_write)
-
-                    if departure.find('p', class_='price-strike'):
-                        original_price = departure.find('p', class_='price-strike').text.strip().replace(',', '')
-                    else:
-                        original_price = actual_price
-                    string_to_write = [trip_name,departure_id,'OriginalPriceUSD',original_price]
-                    csv_writer.writerow(string_to_write)
-                    # print(string_to_write)
-
-                    if departure.find('div', class_='small-message'):
-                        type_smallgroup = 'Small-Group Discovery'
-                        string_to_write = [trip_name,departure_id,'Type',type_smallgroup]
-                        csv_writer.writerow(string_to_write)
-                        # print(string_to_write)
-                    elif departure.find('div', class_='popular-message'):
-                        type_popular = 'Popular'
-                        string_to_write = [trip_name,departure_id,'Type',type_popular]
-                        csv_writer.writerow(string_to_write)
-                        # print(string_to_write)
-
-                    if departure.find('div', class_='listing-status'):
-                        notes = departure.find('div', class_='listing-status').text.strip()
-                    else:
-                        notes = ''
-                    string_to_write = [trip_name,departure_id,'Notes',notes]
-                    csv_writer.writerow(string_to_write)
-                    # print(string_to_write)
-
-                    if re.search( "Not Available", departure.find('div', class_='listing-buttons-contain').text) or notes == '0 Seats Remaining':
-                        available = False
-                        status = 'Not Available'
-                    else:
-                        available = True
-                        status = 'Available'
-                    string_to_write = [trip_name,departure_id,'Available',available]
-                    csv_writer.writerow(string_to_write)
-                    # print(string_to_write)
-                    string_to_write = [trip_name,departure_id,'Status',status]
-                    csv_writer.writerow(string_to_write)
-                    # print(string_to_write)
-
-                    previous_departure_date = departure_date
-
-            except:
-                error_log['{} - US'.format(link)] = 'Missing from Website'
+    for trip in trips:
+        trip.print_deps(file_name)
 
     print('\n\n*** Error Log ***')
     for code, error in error_log.items():
