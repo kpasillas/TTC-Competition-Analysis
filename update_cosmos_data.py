fbafba48e656c3ec41b00b1cda7e0a6daee77460
Kristopher Pasillas <kris@kris-p.net>
2021-07-19 13:00:27 -0700

    Revised update_cosmos_data file to work with Trip and Departure classes

--- update_cosmos_data.py
+++ update_cosmos_data.py
@@ -8,8 +8,10 @@ from bs4 import BeautifulSoup
 from tqdm import tqdm
 import requests
 import re
-import csv
 from datetime import datetime
+
+from trip import Trip
+from departure import Departure
 
 def main():
 
@@ -19,6 +21,7 @@ def main():
     regions_US = []
     trips_US = []
     regions_AU = []
+    trips = []
     error_log = dict()
 
     trip_continents = [
@@ -48,7 +51,6 @@ def main():
 
     regions_US.append('https://www.cosmos.com/Vacations/South-America/')
 
-    # print(regions_US)
 
     for region in tqdm(regions_US):
 
@@ -68,7 +70,6 @@ def main():
         finally:
             driver.quit()
 
-    # print(trips_US)
 
     for continent in tqdm(trip_continents):
 
@@ -88,101 +89,60 @@ def main():
             finally:
                 driver.quit()
 
-    # print(regions_AU)
 
-    with open(file_name, 'a') as new_file:
-        csv_writer = csv.writer(new_file, lineterminator='\n')
+    for trip in tqdm(trips_US):
 
-        field_names = ['Trip Name','DepartureID','field','value']
-        csv_writer.writerow(field_names)
+        departures = []
+        
+        res = requests.get(trip['link'])
+        soup = BeautifulSoup(res.text, 'lxml')
 
-        for trip in tqdm(trips_US):
-
-            res = requests.get(trip['link'])
-            soup = BeautifulSoup(res.text, 'lxml')
-
-            try:
-                trip_name = soup.find("h1").contents[0].text
-                code = soup.find("h1").contents[2].text
-                code = code.strip("()")
-                # print('{} - {}'.format(trip_name, code))
-                previous_departure_date = ''
-                duplicate_departure_count = 0
-
-                for departure in soup.find_all('div', class_='listing'):
-
-                    date_numbers = departure.find('p', class_='date-numbers').text.split()
-                    departure_date = '{:02}-{}-20{}'.format(int(date_numbers[0]), date_numbers[1], date_numbers[2])
-                    # print(departure_date)
-
-                    if departure_date == previous_departure_date:                   # check if duplicate departure
-                        duplicate_departure_count += 1
-                    else:
-                        duplicate_departure_count = 0
-
-                    departure_letter = str(chr(duplicate_departure_count + 97))
-                    day = '{:02}'.format(int(date_numbers[0]))
-                    month = str(chr((datetime.strptime(date_numbers[1], '%b')).month + 64))
-                    year = date_numbers[2][-2:]
-                    op_code = 'Cosmos{}{}'.format(code, year)
-                    departure_code = '{}{}{}{}'.format(day, month, year, departure_letter)
-                    departure_id = '{}-{}'.format(op_code, departure_code)
-                    # print(departure_id)
-
-                    string_to_write = [trip_name,departure_id,'DepartureDate',departure_date]
-                    csv_writer.writerow(string_to_write)
-                    # print(string_to_write)
-
-                    actual_price = departure.find('p', class_='price-actual').text.strip().replace(',', '')
-                    string_to_write = [trip_name, departure_id,'ActualPriceUSD',actual_price]
-                    csv_writer.writerow(string_to_write)
-                    # print(string_to_write)
-
-                    if departure.find('p', class_='price-strike'):
-                        original_price = departure.find('p', class_='price-strike').text.strip().replace(',', '')
-                    else:
-                        original_price = actual_price
-                    string_to_write = [trip_name,departure_id,'OriginalPriceUSD',original_price]
-                    csv_writer.writerow(string_to_write)
-                    # print(string_to_write)
-
-                    if departure.find('div', class_='small-message'):
-                        type_smallgroup = 'Small-Group Discovery'
-                        string_to_write = [trip_name,departure_id,'Type',type_smallgroup]
-                        csv_writer.writerow(string_to_write)
-                        # print(string_to_write)
-                    elif departure.find('div', class_='popular-message'):
-                        type_popular = 'Popular'
-                        string_to_write = [trip_name,departure_id,'Type',type_popular]
-                        csv_writer.writerow(string_to_write)
-                        # print(string_to_write)
-
-                    if departure.find('div', class_='listing-status'):
-                        notes = departure.find('div', class_='listing-status').text.strip()
-                    else:
-                        notes = ''
-                    string_to_write = [trip_name,departure_id,'Notes',notes]
-                    csv_writer.writerow(string_to_write)
-                    # print(string_to_write)
-
-                    if re.search( "Not Available", departure.find('div', class_='listing-buttons-contain').text) or notes == '0 Seats Remaining':
-                        available = False
-                        status = 'Not Available'
-                    else:
-                        available = True
-                        status = 'Available'
-                    string_to_write = [trip_name,departure_id,'Available',available]
-                    csv_writer.writerow(string_to_write)
-                    # print(string_to_write)
-                    string_to_write = [trip_name,departure_id,'Status',status]
-                    csv_writer.writerow(string_to_write)
-                    # print(string_to_write)
-
-                    previous_departure_date = departure_date
+        try:
+            trip_name = soup.find("h1").contents[0].text
+            trip_code = 'Cosmos{}'.format(soup.find("h1").contents[2].text.strip("()"))
             
-            except:
-                error_log['{} - US'.format(link)] = 'Missing from Website'
+            for departure in soup.find_all('div', class_='listing'):
 
+                date_numbers = departure.find('p', class_='date-numbers').text.split()
+                departure_date = '{:02}-{}-20{}'.format(int(date_numbers[0]), date_numbers[1], date_numbers[2])
+                
+                actual_price_usd = departure.find('p', class_='price-actual').text.strip().replace(',', '')
+                if departure.find('p', class_='price-strike'):
+                    original_price_usd = departure.find('p', class_='price-strike').text.strip().replace(',', '')
+                else:
+                    original_price_usd = actual_price_usd
+                
+                if departure.find('div', class_='small-message'):
+                    type = 'Small-Group Discovery'
+                elif departure.find('div', class_='popular-message'):
+                    type = 'Popular'
+                else:
+                    type = ''
+                
+                if departure.find('div', class_='listing-status'):
+                    notes = departure.find('div', class_='listing-status').text.strip()
+                else:
+                    notes = ''
+                
+                if re.search( "Not Available", departure.find('div', class_='listing-buttons-contain').text) or notes == '0 Seats Remaining':
+                    status = 'Not Available'
+                    available = False
+                else:
+                    status = 'Available'
+                    available = True
+
+                new_dep = Departure(date = departure_date, actual_price_usd = actual_price_usd, original_price_usd = original_price_usd, type = type, notes = notes, status = status, available = available)
+                departures.append(new_dep)
+            
+            new_trip = Trip(trip_name, trip_code, departures)
+            trips.append(new_trip)
+        
+        except:
+            error_log['{} - US'.format(link)] = 'Missing from Website'
+
+    for trip in trips:
+        trip.print_deps(file_name)
+    
     print('\n\n*** Error Log ***')
     for code, error in error_log.items():
         print('{}: {}'.format(code, error))
